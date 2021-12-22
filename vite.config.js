925faac5b12c0273f720cc8749d544c07b36576a
Jarosław Filipiak <info@j-filipiak.pl>
2021-12-22 21:57:04 +0100

    utm + weronika

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path');
+
+export default {
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+};
